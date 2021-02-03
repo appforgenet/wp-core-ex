e68fd2d2df1f25a45d763e472424831e18ea2753
Michael Gierczal <info@app-forge.net>
2021-02-03 11:57:03 +0100

    Fixes

--- admin/partials/scripts.php
+++ admin/partials/scripts.php
@@ -23,3 +23,15 @@
     <script type='text/javascript' src="<?= plugin_dir_url( __FILE__ ).'/../../../../../../wp-includes/js/jquery/jquery.js'?>"></script>    
     <script type='text/javascript' src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script type='text/javascript' src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script type='text/javascript'>
+
+(function( $ ) {
+        $('.navbar-toggler').click(function(){
+            if($('#sidebarMenu').is(':visible'))
+                $('#sidebarMenu').hide();
+            else
+                $('#sidebarMenu').show();
+        });
+    })( jQuery );
+
+    </script>
